154ba1d47791c969ae062c5f930b8d72d553f620
Ilya N <1llyanovohatskyy@gmail.com>
2017-11-10 22:59:52 +0200

    Cleaning and Formatting Data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn import tree
 
 train = pd.read_csv('train.csv')  # DataFrame object
 test = pd.read_csv('test.csv')
@@ -42,6 +43,7 @@ print(train["Survived"][train["Child"] == 1].value_counts(normalize=True))
 print(train["Survived"][train["Child"] == 0].value_counts(normalize=True))
 '''
 
+'''
 # Create a copy of test: test_one
 test_one = test
 
@@ -51,4 +53,20 @@ test_one["Survived"] = 0
 # Set Survived to 1 if Sex equals "female" and print the `Survived` column from `test_one`
 test_one["Survived"][test_one["Sex"] == 'female'] = 1
 print(test_one["Survived"])
+'''
+
+# Convert the male and female groups to integer form
+train["Sex"][train["Sex"] == 'male'] = 0
+train["Sex"][train["Sex"] == 'female'] = 1
+# Impute the Embarked variable
+train["Embarked"] = train["Embarked"].fillna("S")
+
+# Convert the Embarked classes to integer form
+train["Embarked"][train["Embarked"] == "S"] = 0
+train["Embarked"][train["Embarked"] == "C"] = 1
+train["Embarked"][train["Embarked"] == "Q"] = 2
+# Print the Sex and Embarked columns
+print(train["Sex"])
+print(train["Embarked"])
+
 

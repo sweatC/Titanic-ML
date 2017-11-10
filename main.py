8a6b7a0d007082fbfc0dad5b865ac6b069fdebc0
Illya Novohatskyy <joker_112@ukr.net>
2017-11-10 16:22:25 +0200

    Does age play role?

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 
 train = pd.read_csv('train.csv')  # DataFrame object
-# print("Shape of training set:", train.shape)
-# print(train.describe())
-
+'''
+print("Shape of training set:", train.shape)
+print(train.describe())
+'''
+'''
 # Passengers that survived vs passengers that passed away
 print(train["Survived"].value_counts())
 
@@ -22,4 +24,16 @@ print(train["Survived"][train["Sex"] == 'male'].value_counts(normalize=True))
 
 # Normalized female survival
 print(train["Survived"][train["Sex"] == 'female'].value_counts(normalize=True))
+'''
+pd.options.mode.chained_assignment = None  # default='warn'
+train["Child"] = float('NaN')
 
+train["Child"][train["Age"] < 18] = 1
+train["Child"][train["Age"] >= 18] = 0
+print(train["Child"])
+
+# Print normalized Survival Rates for passengers under 18
+print(train["Survived"][train["Child"] == 1].value_counts(normalize=True))
+
+# Print normalized Survival Rates for passengers 18 or older
+print(train["Survived"][train["Child"] == 0].value_counts(normalize=True))

02f788703078970eb93b6b08583178c8775c5d46
Ilya N <1llyanovohatskyy@gmail.com>
2017-11-10 16:37:40 +0200

    First prediction

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 train = pd.read_csv('train.csv')  # DataFrame object
+test = pd.read_csv('test.csv')
 '''
 print("Shape of training set:", train.shape)
 print(train.describe())
@@ -25,7 +26,9 @@ print(train["Survived"][train["Sex"] == 'male'].value_counts(normalize=True))
 # Normalized female survival
 print(train["Survived"][train["Sex"] == 'female'].value_counts(normalize=True))
 '''
+
 pd.options.mode.chained_assignment = None  # default='warn'
+'''
 train["Child"] = float('NaN')
 
 train["Child"][train["Age"] < 18] = 1
@@ -37,3 +40,15 @@ print(train["Survived"][train["Child"] == 1].value_counts(normalize=True))
 
 # Print normalized Survival Rates for passengers 18 or older
 print(train["Survived"][train["Child"] == 0].value_counts(normalize=True))
+'''
+
+# Create a copy of test: test_one
+test_one = test
+
+# Initialize a Survived column to 0
+test_one["Survived"] = 0
+
+# Set Survived to 1 if Sex equals "female" and print the `Survived` column from `test_one`
+test_one["Survived"][test_one["Sex"] == 'female'] = 1
+print(test_one["Survived"])
+

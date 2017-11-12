61489efb6cb45614fa5acc336ab898d5b0ab0ebb
Ilya N <1llyanovohatskyy@gmail.com>
2017-11-12 17:29:21 +0200

    A Random Forest analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 train = pd.read_csv('train.csv')  # DataFrame object
 test = pd.read_csv('test.csv')
@@ -144,7 +145,18 @@ my_solution.to_csv('my_solution_three.csv', index_label=["PassengerId"])
 MY_SOLUTUION_THREE_SCORE = 0.65071
 print("Kaggle score using test set is: {}".format(MY_SOLUTUION_THREE_SCORE))'''
 
+# TODO: split up into modules for solutions and data
 
-
+# We want the Pclass, Age, Sex, Fare,SibSp, Parch, and Embarked variables
+features_forest = train[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+# Building and fitting my_forest
+forest = RandomForestClassifier(max_depth=10, min_samples_split=2, n_estimators=100, random_state=1)
+my_forest = forest.fit(features_forest, target)
+# Print the score of the fitted random forest
+print("Random Forest score for test set: {}".format(my_forest.score(features_forest, target)))
+# Compute predictions on our test set features then print the length of the prediction vector
+test_features = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+pred_forest = my_forest.predict(test_features)
+print("Predict forest length: {}".format(len(pred_forest)))
 
 
